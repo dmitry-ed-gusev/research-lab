434f45b9dd99a5780928693e35ea38f309482e47
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2016-11-27 21:50:02 +0300

    Created scripts suite: update system, install base software, install java, other things.

--- scripts/shell/system/_system-update.sh
+++ scripts/shell/system/_system-update.sh
@@ -2,16 +2,18 @@
 #
 # =============================================================================
 #   This script updates system (Debian-based). After update <autoremove> will 
-#   be executed (with apt-get) - for removing unnecessary packages.
+#   be executed (with apt-get) - for removing unnecessary packages. Script
+#   is a part of scripts suite and shouldn't be called by itself. Use
+#   <./mysys.sh -update>
 #
 #   WARNING! Script should not be started as user 'root' (with command like: 
-#   sudo ./<script_name>)! Script will ask for such priveleges, if necessary.
+#   sudo ./<script_name>)! Script will ask for such privileges, if necessary.
 #  
-#   Created:  Gusev Dmitry, 27.10.2015
-#   Modified: Gusev Dmitry, 11.03.2016
+#   Created:  Gusev Dmitry, 26.11.2016
+#   Modified:
 # =============================================================================
 
-# -- Update system quietly . If you uncomments /dev/null, you don't see any info.
+# -- Update system quietly. If you remove comments from /dev/null, you won't see any info.
 sudo /usr/bin/apt-get -qy update # > /dev/null
 sudo /usr/bin/apt-get -qy upgrade # > /dev/null
 sudo /usr/bin/apt-get -qy dist-upgrade # > dev/null
@@ -20,4 +22,6 @@ sudo /usr/bin/apt-get -qy dist-upgrade # > dev/null
 sudo /usr/bin/apt-get -qy autoremove
 
 # -- Reboot system aftre updating
-sudo reboot now
+if [ "$REBOOT_AFTER_UPDATE" == "YES" ]; then
+    sudo reboot now
+fi
