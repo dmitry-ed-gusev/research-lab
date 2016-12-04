10830fd78c263ddb319fb773feabd8581ff0f8e2
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2016-12-04 22:17:45 +0300

    Some calculator improvements.

--- utilities/src/main/java/gusev/dmitry/jtils/calc/CalcButton.java
+++ utilities/src/main/java/gusev/dmitry/jtils/calc/CalcButton.java
@@ -4,23 +4,24 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 
 /**
+ * Calculator button.
  * @author Gusev Dmitry (gusevd)
  * @version 1.0 (DATE: 03.10.12)
 */
 
 public class CalcButton extends JButton {
 
+    /** Default constructor. */
     public CalcButton(String text, ActionListener actionListener) {
         super(text);
         if (actionListener == null) {
-            throw new IllegalStateException("Action listener for JButton can't be null!");
+            throw new IllegalStateException("Action listener for CalcButton can't be null!");
         }
-        // register action listener
-        this.addActionListener(actionListener);
-        // all our calc buttons can't gain focus
-        this.setFocusable(false);
-        // set action command
-        this.setActionCommand(text);
+
+        this.addActionListener(actionListener); // register action listener
+        this.setFocusable(false);               // no one calc button can't gain focus
+        this.setActionCommand(text);            // set action command
+
     }
 
 }
