00ae8256851659fe561b0211c033bdc14ae195dc
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2017-03-07 14:01:27 +0300

    Added some python files (education -> learning python)

---
+++ python/research/messages.py
@@ -0,0 +1,13 @@
+print ("Hello!")
+
+msg = "\nfirst message  "
+# as is
+print("not stripped -> [" + msg + "]")
+print("stripped -> [" + msg.strip() + "]")
+print(msg.title())
+
+msg = "\nsecond\n\tone"
+print(msg)
+print (msg.title())
+# print(msg.count(msg))
+
--- python/utilities/myutils.py
+++ python/utilities/myutils.py
@@ -8,9 +8,11 @@ __author__ = 'gusevd'
 
 def to_upper_case(path):
     """
-    Rename all files in a directory to upper case
+    Rename all files in a given directory to upper case
     :return: nothing
     """
+
+    print "to_upper_case() is working."
 
     # -- go trough path recursively and search all files
     for (dirpath, dirnames, filenames) in walk(unicode(path)):
@@ -23,7 +25,30 @@ def to_upper_case(path):
             rename(before, after)
             print 'to upper case:', before, '->', after
 
+
+def to_title(path):
+    """
+    Rename all files in a given directory to title case (all
+    words with uppercase first letter)
+    :return: nothing
+    """
+
+    print "to_title() is working."
+
+    # -- go trough path recursively and search all files
+    for (dirpath, dirnames, filenames) in walk(unicode(path)):
+
+        # -- iterate over found files in concrete directory and rename them (to upper case)
+        prefix = dirpath + '\\'
+        for filename in filenames:
+            before = prefix + filename
+            after  = prefix + filename.title()
+            rename(before, after)
+            print 'to title case:', before, '->', after
+
+
 # -- execute, if runned sigle or imported by other module
 if __name__ == '__main__':
-    print '[myutils] working.'
-    to_upper_case('c:\\temp')
+    print '[myutils] main is working.'
+    # to_upper_case('c:\\temp')
+    to_title("c:\\temp")
