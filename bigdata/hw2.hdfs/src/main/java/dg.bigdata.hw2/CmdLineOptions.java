5911438f4fcaed99056b08863801946c28abf74b
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2017-05-29 21:11:43 +0300

    BigData HW#2. Dev step (cmd line option added).

--- bigdata/hw2.hdfs/src/main/java/dg.bigdata.hw2/CmdLineOptions.java
+++ bigdata/hw2.hdfs/src/main/java/dg.bigdata.hw2/CmdLineOptions.java
@@ -1,10 +1,12 @@
 package dg.bigdata.hw2;
 
+import gusev.dmitry.jtils.utils.CmdLineOption;
+
 /**
  * Cmd line options enumeration for HDFS utility
  * Created by gusevdm on 5/25/2017.
  */
-public enum CmdLineOptions {
+public enum CmdLineOptions implements CmdLineOption {
 
     CAT_FILE_BY_URL("-catFileByUrl");
 
@@ -12,6 +14,11 @@ public enum CmdLineOptions {
 
     CmdLineOptions(String optionName) {
         this.optionName = optionName;
+    }
+
+    @Override
+    public String getName() {
+        return this.optionName;
     }
 
 }
