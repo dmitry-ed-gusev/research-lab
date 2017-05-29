f2b8c7a41428b3904a895948cea5cb4533b8eddd
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2017-05-29 22:32:43 +0300

    CMD line - add suggestions for improvements.

--- utilities/src/main/java/gusev/dmitry/jtils/utils/CmdLineOption.java
+++ utilities/src/main/java/gusev/dmitry/jtils/utils/CmdLineOption.java
@@ -5,6 +5,23 @@ package gusev.dmitry.jtils.utils;
  * Created by vinnypuhh on 28.05.2017.
  */
 
+// todo: add getDescription() method
+// todo: add getHelp(String app_desc) method:
+/*
+    public static String getHelpText() {
+        StringBuilder result = new StringBuilder();
+
+        result.append(APP_DESCRIPTION); // add description
+
+        for (CmdLineOption option : CmdLineOption.values()) { // add options
+            result.append(option.getOptionDesc()).append("\n");
+        }
+
+        return result.toString();
+    }
+     */
+// todo: add abstract class implementation
+
 public interface CmdLineOption {
 
     /** Return option name. */
