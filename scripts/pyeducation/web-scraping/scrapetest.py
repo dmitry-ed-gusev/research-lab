84df516eb1def9de5df21c89f870c559b471ca02
gusevdm <dmitrii.gusev@merck.com>
2017-08-30 17:02:38 +0300

    Added proxy support to scraper.

--- scripts/pyeducation/web-scraping/scrapetest.py
+++ scripts/pyeducation/web-scraping/scrapetest.py
@@ -1,31 +1,55 @@
+"""
+    https://stackoverflow.com/questions/1450132/proxy-with-urllib2
+"""
+
 from urllib2 import urlopen
+from urllib2 import build_opener
+from urllib2 import install_opener
 from urllib2 import HTTPError
+from urllib2 import ProxyHandler
 from bs4 import BeautifulSoup
 
-# https://stackoverflow.com/questions/1450132/proxy-with-urllib2
-def get_html(url):
+
+def get_html(url, http_proxy="", https_proxy=""):
     """
     Returns html by specified url
+    :param https_proxy:
+    :param http_proxy:
     :param url: target url for getting the whole html
     :return:
     """
     if not url:   # fast check and return
         return None
     try:
+        # if specified proxy - set it
+        if http_proxy or https_proxy:
+            print "Using proxies: http [{}], https [{}].".format(http_proxy, https_proxy)
+            # set up proxy handler
+            proxy = ProxyHandler({
+                'http': http_proxy,
+                'https': https_proxy
+            })
+            # install proxy handler for urllib2
+            opener = build_opener(proxy)
+            install_opener(opener)
+
         return urlopen(url)
+
     except HTTPError as e:
         print "HTTP Error: {}".format(e)
         return None
 
 
-def get_page_title(url):
+def get_page_title(url, http_proxy="", https_proxy=""):
     """
     Returns title from web-page (html) from specified url
+    :param https_proxy:
+    :param http_proxy:
     :param url:
     :return:
     """
     # read html form url
-    html = get_html(url)
+    html = get_html(url, http_proxy, https_proxy)
     if not html:
         print "Url [{}] returned empty page!".format(url)
         return None
@@ -42,7 +66,7 @@ def get_page_title(url):
     return page_title
 
 # get and print page title
-title = get_page_title("http://www.pythonscraping.com/pages/page1.html")
+title = get_page_title("http://www.pythonscraping.com/pages/page1.html", http_proxy='webproxy.merck.com:8080', https_proxy='webproxy.merck.com:8080')
 
 if not title:
     print("Title could not be found")
