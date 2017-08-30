de32936f96613fa879c7865a0db076cd936d1c53
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2017-08-30 22:53:18 +0300

    Minor scraper changes.

--- scripts/pyeducation/web-scraping/scrapetest.py
+++ scripts/pyeducation/web-scraping/scrapetest.py
@@ -8,10 +8,12 @@ from urllib2 import install_opener
 from urllib2 import HTTPError
 from urllib2 import ProxyHandler
 from bs4 import BeautifulSoup
+import re
 
 # some useful constants
 HTML_PARSER = "html.parser"
 PROXY_SERVER = "webproxy.merck.com:8080"
+REGEX_EMAIL = "[A-Za-z0-9\._+]+@[A-Za-z]+\.(com|org|edu|net|ru)"
 
 
 def get_bs_object(url, http_proxy="", https_proxy=""):
@@ -60,13 +62,28 @@ def get_page_data(url, http_proxy="", https_proxy=""):
             return
 
         # print page title
-        print "Page title -> {}". format(bs_obj.body.h1)
+        # print "Page title -> {}". format(bs_obj.body.h1)
 
         # print all persons names
-        names_list = bs_obj.findAll("span", {"class" : "green"})
+        # names_list = bs_obj.findAll("span", {"class" : "green"})
         # names_list = bs_obj.findAll(class_="green")
-        for name in names_list:
-            print name.get_text()
+        # for name in names_list:
+        #    print name.get_text()
+
+        # print siblings
+        # for sibling in bs_obj.find("table", {"id" : "giftList"}).tr.next_siblings:
+        #    print sibling
+
+        # print "===> {}".format(bs_obj.find("img", {"src": "../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
+
+        # print all images addresses
+        images = bs_obj.findAll("img", {"src": re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+        for image in images:
+            print image["src"]
+
+        tags = bs_obj.findAll(lambda tag: len(tag.attrs) == 2)
+        for tag in tags:
+            print "tag -> {}".format(tag)
 
     except AttributeError as e:
         print "HTML Attribute Error: {}".format(e)
@@ -79,4 +96,11 @@ page3 = "http://www.pythonscraping.com/pages/page3.html"
 
 # get and print page title
 # get_page_data(page1, http_proxy=PROXY_SERVER, https_proxy=PROXY_SERVER)
-get_page_data(page1)
+#get_page_data(page1)
+#print
+
+#get_page_data(page2)
+#print
+
+get_page_data(page3)
+print
