0dafe3efb0a1abe4c0a735a63a693a5b3a0a372a
gusevdm <dmitrii.gusev@merck.com>
2017-08-30 14:34:44 +0300

    Minor changes.

--- scripts/pyeducation/web-scraping/scrapetest.py
+++ scripts/pyeducation/web-scraping/scrapetest.py
@@ -2,6 +2,21 @@ from urllib2 import urlopen
 from urllib2 import HTTPError
 from bs4 import BeautifulSoup
 
+# https://stackoverflow.com/questions/1450132/proxy-with-urllib2
+def get_html(url):
+    """
+    Returns html by specified url
+    :param url: target url for getting the whole html
+    :return:
+    """
+    if not url:   # fast check and return
+        return None
+    try:
+        return urlopen(url)
+    except HTTPError as e:
+        print "HTTP Error: {}".format(e)
+        return None
+
 
 def get_page_title(url):
     """
@@ -9,12 +24,13 @@ def get_page_title(url):
     :param url:
     :return:
     """
-    try:
-        # read html from specified url
-        html = urlopen(url)
-    except HTTPError as e:
-        print "HTTP Error: {}".format(e)
+    # read html form url
+    html = get_html(url)
+    if not html:
+        print "Url [{}] returned empty page!".format(url)
         return None
+
+    # getting content (title) from returned html
     try:
         # create BeautifulSoup object
         bs_obj = BeautifulSoup(html.read(), "html.parser")
