e275db22304405ef434f595e32d94f1bef6baeb7
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2017-08-30 20:01:00 +0300

    Minor scraper changes.

--- scripts/pyeducation/web-scraping/scrapetest.py
+++ scripts/pyeducation/web-scraping/scrapetest.py
@@ -9,8 +9,12 @@ from urllib2 import HTTPError
 from urllib2 import ProxyHandler
 from bs4 import BeautifulSoup
 
+# some useful constants
+HTML_PARSER = "html.parser"
+PROXY_SERVER = "webproxy.merck.com:8080"
 
-def get_html(url, http_proxy="", https_proxy=""):
+
+def get_bs_object(url, http_proxy="", https_proxy=""):
     """
     Returns html by specified url
     :param https_proxy:
@@ -33,14 +37,14 @@ def get_html(url, http_proxy="", https_proxy=""):
             opener = build_opener(proxy)
             install_opener(opener)
 
-        return urlopen(url)
+        return BeautifulSoup(urlopen(url).read(), HTML_PARSER)
 
     except HTTPError as e:
         print "HTTP Error: {}".format(e)
         return None
 
 
-def get_page_title(url, http_proxy="", https_proxy=""):
+def get_page_data(url, http_proxy="", https_proxy=""):
     """
     Returns title from web-page (html) from specified url
     :param https_proxy:
@@ -48,27 +52,31 @@ def get_page_title(url, http_proxy="", https_proxy=""):
     :param url:
     :return:
     """
-    # read html form url
-    html = get_html(url, http_proxy, https_proxy)
-    if not html:
-        print "Url [{}] returned empty page!".format(url)
-        return None
-
-    # getting content (title) from returned html
     try:
-        # create BeautifulSoup object
-        bs_obj = BeautifulSoup(html.read(), "html.parser")
-        # get title from h1 tag
-        page_title = bs_obj.body.h1
+        # read url and get BeautifulSoup object
+        bs_obj = get_bs_object(url, http_proxy, https_proxy)
+        if not bs_obj:
+            print "Url [{}] returned empty content!".format(url)
+            return
+
+        # print page title
+        print "Page title -> {}". format(bs_obj.body.h1)
+
+        # print all persons names
+        names_list = bs_obj.findAll("span", {"class" : "green"})
+        # names_list = bs_obj.findAll(class_="green")
+        for name in names_list:
+            print name.get_text()
+
     except AttributeError as e:
         print "HTML Attribute Error: {}".format(e)
-        return None
-    return page_title
+
+# ===============================================
+
+page1 = "http://www.pythonscraping.com/pages/page1.html"
+page2 = "http://www.pythonscraping.com/pages/warandpeace.html"
+page3 = "http://www.pythonscraping.com/pages/page3.html"
 
 # get and print page title
-title = get_page_title("http://www.pythonscraping.com/pages/page1.html", http_proxy='webproxy.merck.com:8080', https_proxy='webproxy.merck.com:8080')
-
-if not title:
-    print("Title could not be found")
-else:
-    print(title)
+# get_page_data(page1, http_proxy=PROXY_SERVER, https_proxy=PROXY_SERVER)
+get_page_data(page1)
