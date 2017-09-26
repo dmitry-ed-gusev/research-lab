6eda8da3c3578ce0683d252bf49a9baaeb23cca9
gusevdm <dmitrii.gusev@merck.com>
2017-09-26 09:13:14 +0300

    Minor fixes.

--- scripts/mypygame/settings.py
+++ scripts/mypygame/settings.py
@@ -1,31 +1,32 @@
 class Settings(object):
     """A class to store all settings for Alien Invasion."""
 
-    def __init__(self):
+    def __init__(self, config_file=None):
         """Initialize the game's static settings."""
-        # Screen settings.
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
-        
-        # Ship settings.
-        self.ship_limit = 3
-            
-        # Bullet settings.
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 3
-        
-        # Alien settings.
-        self.fleet_drop_speed = 10
-            
-        # How quickly the game speeds up.
-        self.speedup_scale = 1.1
-        # How quickly the alien point values increase.
-        self.score_scale = 1.5
-    
-        self.initialize_dynamic_settings()
+        print "Settings -> __init()__"
+
+        if not config_file:
+            print "No config file specified. Loading default settings."
+            # Screen settings.
+            self.screen_width = 1200
+            self.screen_height = 800
+            self.bg_color = (230, 230, 230)
+            # Ship settings.
+            self.ship_limit = 3
+            # Bullet settings.
+            self.bullet_width = 3
+            self.bullet_height = 15
+            self.bullet_color = 60, 60, 60
+            self.bullets_allowed = 3
+            # Alien settings.
+            self.fleet_drop_speed = 10
+            # How quickly the game speeds up.
+            self.speedup_scale = 1.1
+            # How quickly the alien point values increase.
+            self.score_scale = 1.5
+            # init some dynamic settings
+            self.initialize_dynamic_settings()
+        #elif
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
@@ -33,10 +34,8 @@ class Settings(object):
         self.ship_speed_factor = 1.5
         self.bullet_speed_factor = 3
         self.alien_speed_factor = 1
-        
         # Scoring.
         self.alien_points = 50
-    
         # fleet_direction of 1 represents right, -1 represents left.
         self.fleet_direction = 1
         
