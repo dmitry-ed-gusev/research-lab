f96f1f129c71fb44662d52f993c5451cb598e3a9
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2017-10-16 22:47:29 +0300

    Python - some logging experiments...

--- scripts/pyeducation/mylogging.py
+++ scripts/pyeducation/mylogging.py
@@ -1,14 +1,15 @@
 import logging as log
 
-# log message format
+# log message format (see https://docs.python.org/2/library/logging.html#logging.LogRecord)
 # %d{dd/MM/yyyy HH:mm:ss} %-5p [%c{1}:%L] %m%n
-fmt = '%(asctime)s %(levelname)s %(lineno)s %(message)s'
+fmt = '%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)s] %(message)s'
 
 # do some logging config
-log.basicConfig(level=log.DEBUG, filename="mylog.log", format=fmt)
+# log.basicConfig(level=log.DEBUG, filename="mylog.log", format=fmt)
+log.basicConfig(level=log.DEBUG, format=fmt, datefmt='%d/%m/%Y %H:%M:%S')
 
 # create loggeing object
-log = log.getLogger("dmitrii")
+log = log.getLogger(__name__)
 
 # write some messages
 log.debug("zzzzz")
