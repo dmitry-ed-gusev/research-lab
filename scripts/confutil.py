2e4e52caff7e095b8d620db5701b8c20d590aa99
gusevdm <dmitrii.gusev@merck.com>
2018-03-18 22:46:44 +0300

    Some dev steps for Confluence utility.

--- scripts/confutil.py
+++ scripts/confutil.py
@@ -5,14 +5,49 @@ python -mjson.tool
 
 Resources:
 https://github.com/pycontribs/confluence
+https://avleonov.com/2018/01/18/confluence-rest-api-for-reading-and-updating-wiki-pages/
 """
 
 import argparse
 import logging
+import json
+import requests
 import pylib.common_constants as myconst
 from pylib.pyutilities import setup_logging
 
+
+def get_page_json(page_id, expand=False):
+    if expand:
+        suffix = "?expand=" + expand  # body.storage
+    else:
+        suffix = ""
+
+    url="https://share.merck.com/rest/api/content/" + page_id + suffix
+    response = requests.get(url, auth=('gusevdm', 'Vinnypuhh33!'))
+    response.encoding = "utf8"
+    return json.loads(response.text)
+
+
+def set_page_json(page_id,json_content):
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.put("https://confluence.corporation.com/rest/api/content/" + page_id,
+                            headers=headers, data=json.dumps(json_content),
+                            #auth=(user, password)
+                            auth=('', ''))
+    return(response.text)
+
+
+# -------------------------------
 setup_logging()
 # get module-level logger
 log = logging.getLogger(myconst.LOGGER_NAME_GITUTIL)
 log.info("Starting GIT Utility...")
+
+json_data = get_page_json("284590357", "body.storage")  # with body content
+# json_data = get_page_json("284590357", '')  # without body content
+print json_data
+print 'Title -> ', json_data['title']
+print 'Body storage value -> ', json_data['body']['storage']['value']
