3c47565920778bd1a6f79f16bf1359b8f2cabf9d
gusevdmi <dmitrii.eduardovitch.gusev@merck.com>
2018-05-20 22:34:55 +0200

    Added logical DataSet object and helper class.

---
+++ datatex-connector/src/main/java/gusevdm/luxms/DataSet.java
@@ -0,0 +1,181 @@
+package gusevdm.luxms;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/** Logical representation of LuxMS BI dataset. */
+// todo: implement BUILDER pattern???
+
+public class DataSet {
+
+    // common constants for dataset JSON
+    public static final String DS_ID               = "id";
+    public static final String DS_GUID             = "guid";
+    public static final String DS_PARENT_GUID      = "parent_guid";
+    public static final String DS_DESCRIPTION      = "description";
+    public static final String DS_TITLE            = "title";
+    public static final String DS_IS_VISIBLE       = "is_visible";
+    public static final String DS_SCHEMA_NAME      = "schema_name";
+    public static final String DS_POST_PROCESS_SQL = "postprocess_sql";
+    public static final String DS_OWNER_USER_ID    = "owner_user_id";
+    public static final String DS_IS_ARCHIVE       = "is_archive";
+
+    // internal object state
+    private long    id;
+    private String  guid;
+    private String  parentGuid;
+    private String  description;
+    private String  title;
+    private boolean isVisible;
+    private String  schemaName;
+    private String  postProcessSql;
+    private String  ownerUser;
+    private boolean isArchive;
+
+    /***/
+    public DataSet(long id, String description, String title) {
+        this.id = id;
+        this.description = description;
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getParentGuid() {
+        return parentGuid;
+    }
+
+    public void setParentGuid(String parentGuid) {
+        this.parentGuid = parentGuid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean visible) {
+        isVisible = visible;
+    }
+
+    public String getSchemaName() {
+        return schemaName;
+    }
+
+    public void setSchemaName(String schemaName) {
+        this.schemaName = schemaName;
+    }
+
+    public String getPostProcessSql() {
+        return postProcessSql;
+    }
+
+    public void setPostProcessSql(String postProcessSql) {
+        this.postProcessSql = postProcessSql;
+    }
+
+    public String getOwnerUser() {
+        return ownerUser;
+    }
+
+    public void setOwnerUser(String ownerUser) {
+        this.ownerUser = ownerUser;
+    }
+
+    public boolean isArchive() {
+        return isArchive;
+    }
+
+    public void setArchive(boolean archive) {
+        isArchive = archive;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SIMPLE_STYLE)
+                .append("id", id)
+                .append("guid", guid)
+                .append("parentGuid", parentGuid)
+                .append("description", description)
+                .append("title", title)
+                .append("isVisible", isVisible)
+                .append("schemaName", schemaName)
+                .append("postProcessSql", postProcessSql)
+                .append("ownerUser", ownerUser)
+                .append("isArchive", isArchive)
+                .toString();
+    }
+
+}
+
+/*
+// dataset JSON object
+{
+        //"parent_guid":null,
+        "period_spans":{
+
+        },
+        //"postprocess_sql":null,
+        //"description":"My New Own Dataset for temp purposes...",
+        //"title":"my_dataset",
+        "is_db_ready":0,
+        "sync_cfg":{
+
+        },
+        "src_image_type":null,
+        //"id":8,
+        "sqlite_snapshot_id":null,
+        "images":{
+
+        },
+        "ui_cfg":{
+
+        },
+        //"is_visible":1,
+        "depends_on":[
+
+        ],
+        //"owner_user_id":null,
+        "logged_since":null,
+        //"schema_name":"ds_8",
+        "server_id":0,
+        "sqlite_serial":null,
+        "version":"3.0",
+        "head_dataset_id":null,
+        "srt":2147483647,
+        "serial":"2018-05-20T23:58:02.359467+03:00",
+        //"is_archive":0,
+        //"guid":"086cff47-9e61-41d9-bcaa-5a3c83492adf",
+        "config":{
+
+        }
+} */
---
+++ datatex-connector/src/main/java/gusevdm/luxms/LuxMSHelper.java
@@ -0,0 +1,39 @@
+package gusevdm.luxms;
+
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static gusevdm.luxms.DataSet.*;
+
+/** Some helpers methods for LuxMS client. */
+
+public final class LuxMSHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LuxMSHelper.class);
+
+    private LuxMSHelper() {}
+
+    /***/
+    public static DataSet parseDataSet(JSONObject json) {
+        LOGGER.debug("LuxMSHelper.parseDataSet() is working.");
+
+        if (json == null) {
+            throw new IllegalStateException("Received JSON object is NULL!");
+        }
+
+        // create dataset
+        DataSet dataSet = new DataSet(Long.parseLong(json.get(DS_ID).toString()),
+                json.get(DS_DESCRIPTION).toString(), json.get(DS_TITLE).toString());
+        // set other parameters
+        dataSet.setVisible(Integer.parseInt(json.get(DS_IS_VISIBLE).toString()) == 1);
+        dataSet.setArchive(Integer.parseInt(json.get(DS_IS_ARCHIVE).toString()) == 1);
+        dataSet.setGuid(json.get(DS_GUID).toString());
+        dataSet.setOwnerUser(json.get(DS_OWNER_USER_ID) == null ? null : json.get(DS_OWNER_USER_ID).toString());
+        dataSet.setParentGuid(json.get(DS_PARENT_GUID) == null ? null : json.get(DS_PARENT_GUID).toString());
+        dataSet.setPostProcessSql(json.get(DS_POST_PROCESS_SQL) == null ? null : json.get(DS_POST_PROCESS_SQL).toString());
+        dataSet.setSchemaName(json.get(DS_SCHEMA_NAME).toString());
+
+        return dataSet;
+    }
+
+}
