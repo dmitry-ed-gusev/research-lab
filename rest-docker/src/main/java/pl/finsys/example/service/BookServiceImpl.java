cbbf23ebffd5472bb7222ee7628a5f7f429fd977
Gusev, Dmitrii Eduardovitch <dmitrii@merck.com>
2019-03-15 21:43:21 +0100

    Added [rest-docker] example with Spring Boot REST service, dockerised, from book "Docker for Java Developers". Minor refactorings. Code is broken (cannot be compiled) - used old version of Spring DPA. Refactoring is needed.

---
+++ rest-docker/src/main/java/pl/finsys/example/service/BookServiceImpl.java
@@ -0,0 +1,68 @@
+package pl.finsys.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import pl.finsys.example.domain.Book;
+import pl.finsys.example.repository.BookRepository;
+import pl.finsys.example.service.exception.BookAlreadyExistsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Validated
+public class BookServiceImpl implements BookService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookServiceImpl.class);
+    private final BookRepository repository;
+
+    @Autowired
+    public BookServiceImpl(final BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    @Transactional
+    public Book saveBook(@NotNull @Valid final Book book) {
+        LOGGER.debug("Creating {}", book);
+
+        // create example from book - for query by example
+        Example<Book> bookExample = Example.of(new Book(book.getId(), null, null));
+        // find book by 'query by example'
+        Optional<Book> bookOptional = repository.findOne(bookExample);
+
+        if (bookOptional.isPresent()) {
+            throw new BookAlreadyExistsException(
+                    String.format("There already exists a book with id=%s", book.getId()));
+        }
+
+        return repository.save(book);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Book> getList() {
+        LOGGER.debug("Retrieving the list of all users");
+        return repository.findAll();
+    }
+
+    @Override
+    public Book getBook(Long bookId) {
+        return repository.findOne(bookId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteBook(final Long bookId) {
+        LOGGER.debug("deleting {}", bookId);
+        repository.delete(bookId);
+    }
+
+}
