b1c1613b5e414d66204e32b6ff0acb1dd886b9a1
Gusev, Dmitrii Eduardovitch <dmitrii@merck.com>
2019-03-19 16:53:33 +0100

    Make app [rest-springboot-docker] working.

--- rest-springboot-docker/src/main/java/pl/finsys/example/BookstoreApplication.java
+++ rest-springboot-docker/src/main/java/pl/finsys/example/BookstoreApplication.java
@@ -3,7 +3,7 @@ package pl.finsys.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"pl.finsys.example"})
 public class BookstoreApplication {
 
     public static void main(final String[] args) {
