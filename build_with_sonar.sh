ec73e789880a6b45a462b98df7c0186cee2f82b7
gusevdmi <dmitrii@merck.com>
2019-05-20 00:01:09 +0300

    Added build scripts for the project.

---
+++ build_with_sonar.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+###############################################################################
+#
+#       Script builds project and run Sonar. Script is doing following:
+#         * builds project with maven (using profile env-prod-all)
+#         * using docker-composer start up docker sonar environment
+#         * execute sonar analysis with maven (mvn sonar:sonar)
+#
+#       Created:  Dmitrii Gusev, 19.05.2019
+#       Modified:
+#
+###############################################################################
+
+# build project with maven
+mvn clean install -s settings_empty.xml -Penv-prod-all
+
+# using docker composer start up sonar environment
+docker-compose up
+
+# execute sonar check for the project
+mvn sonar:sonar -s settings_empty.xml -Penv-prod-all
