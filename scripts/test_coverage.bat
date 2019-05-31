bb2b95676f5f503a5beb1769f86ecfb8db627fd1
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2019-05-31 16:06:20 +0200

    Minor fixes for win os.

--- scripts/test_coverage.bat
+++ scripts/test_coverage.bat
@@ -1,3 +1,4 @@
-pip install nose2 cov-core coverage pyyaml mock --proxy webproxy.merck.com:8080
+rem pip install nose2 cov-core coverage pyyaml mock --proxy webproxy.merck.com:8080
+rem pip install nose2 cov-core coverage pyyaml mock
 rem set PYTHONPATH=./target/dependency;./pymocks
-python -m nose2 -v --plugin nose2.plugins.junitxml -X --with-coverage --coverage-report html xml
+rem python -m nose2 -v --plugin nose2.plugins.junitxml -X --with-coverage --coverage-report html xml
