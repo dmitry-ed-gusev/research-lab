8b2a5fcdb614300a5362c9f3649b27f2834c1272
gusevdmi <dmitrii@merck.com>
2019-06-03 11:44:09 +0300

    Tests improvements for [pyutilities] module.

--- pyutilities/test_with_coverage.sh
+++ pyutilities/test_with_coverage.sh
@@ -3,11 +3,13 @@
 ###############################################################################
 #
 #   Execute unit tests for pyutilities module with generating coverage report.
+#   Unit tests for linux/unix OS.
 #
-#   Created: Dmitrii GUsev, 17.05.2019
-#   Modified:
+#   Created:  Dmitrii Gusev, 17.05.2019
+#   Modified: Dmitrii Gusev, 03.06.2019
 #
 ###############################################################################
+
 
 # create virtual environment and make it relocatable
 virtualenv .venv
@@ -19,17 +21,14 @@ source .venv/bin/activate
 # install necessary testing dependencies
 pip install nose2 pyyaml mock xlrd
 
+# setup PYTHONPATH variable (if necessary)
 #PYTHONPATH=${WORKSPACE}/target/dependency:$PYTHONPATH
 #PYTHONPATH=${WORKSPACE}/pymocks:$PYTHONPATH
 #export PYTHONPATH
 
+# run unit tests with coverage
 python -m nose2 -v -s pyutilities/tests --plugin nose2.plugins.junitxml -X --with-coverage --coverage pyutilities \
     --coverage-report xml --coverage-report html
 
 # deactivate virtual environment (exit)
 deactivate
-
-#python -m nose2 -v -s tests --plugin nose2.plugins.junitxml -X --with-coverage --coverage pipeline --coverage-report xml
-#pip install robotframework
-#pip install robotframework-sshlibrary
-#robot --dryrun --variablefile ${WORKSPACE}/tests/robot/environments/dev_env.py --variable ROOTDIR:${WORKSPACE}/tests/robot tests/robot/pipeline_tests/atomic
