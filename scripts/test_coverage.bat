cf462f9cc88f1a11f6692281af68500e9a488287
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2019-06-04 12:06:49 +0200

    Added run for unit tests with coverage for module [scripts] on build on windows os.

--- scripts/test_coverage.bat
+++
@@ -1,4 +0,0 @@
-rem pip install nose2 cov-core coverage pyyaml mock --proxy webproxy.merck.com:8080
-rem pip install nose2 cov-core coverage pyyaml mock
-rem set PYTHONPATH=./target/dependency;./pymocks
-rem python -m nose2 -v --plugin nose2.plugins.junitxml -X --with-coverage --coverage-report html xml
