1ee30cd92916f72412a06bfaff717a733716605f
Dmitrii Gusev <dmitrii.gusev@broadcom.com>
2022-11-14 17:44:05 +0100

    Module 05 is done.

--- dbms-dwh/dwh-specialization/Course 1 - DB Mgmt Essentials/Problems. Module 5/3. assignment solution.sql
+++ dbms-dwh/dwh-specialization/Course 1 - DB Mgmt Essentials/Problems. Module 5/3. assignment solution.sql
@@ -42,6 +42,9 @@ delete from customer where custno = 'D101';
 
 ---------- Module 05: Assignement Solution ---------- 
 
+
+--- SELECT statements Problems
+
 -- #1
 select eventrequest.eventno, dateheld, count(planno) planscount from eventrequest
   inner join eventplan on eventrequest.eventno = eventplan.eventno
@@ -64,11 +67,88 @@ select eventrequest.eventno, dateheld, status, estcost from eventrequest
   where empname = 'Mary Manager'
   and facname = 'Basketball arena';
   
-
 -- #4
+select eventplanline.planno, lineno, resname, resourcecnt, locname, timestart, timeend from eventplanline
+  inner join resourcetbl on eventplanline.resno = resourcetbl.resno
+  inner join location on eventplanline.locno = location.locno
+  inner join facility on location.facno = facility.facno
+  inner join eventplan on eventplanline.planno = eventplan.planno
+  where facname = 'Basketball arena'
+  and activity = 'Operation'
+  and workdate between '01-Oct-2022' and '31-Dec-2022';
 
 -- #5
+select eventplanline.planno, sum(resourcecnt * rate) as resourcecost from eventplanline
+  inner join resourcetbl on eventplanline.resno = resourcetbl.resno
+  inner join eventplan on eventplanline.planno = eventplan.planno
+  where workdate between '01-Dec-2022' and '31-Dec-2022'
+  group by eventplanline.planno
+  having sum(resourcecnt * rate) > 50;
 
--- #6
 
--- #7
+--- Database Modification Problems
+
+-- #1
+insert into facility values ('SP111', 'Swimming Pool');
+
+-- #2
+insert into location values ('D01', 'SP111', 'Door');
+
+-- #3
+insert into location values ('D02', 'SP111', 'Locker Room');
+
+-- #4
+update location set locname = 'Gate' 
+  where locname = 'Door';
+
+-- #5
+delete from location 
+  where locno in ('D01', 'D02');
+
+
+--- SQL Statements with Errors and Poor Formatting
+
+-- #1 (syntax, redundancy, semantic - semantic)
+SELECT eventrequest.eventno, dateheld, status, estcost
+FROM eventrequest, employee, facility, eventplan
+WHERE estaudience > 5000
+ AND eventplan.empno = employee.empno
+ AND eventrequest.facno = facility.facno
+ AND eventplan.eventno = eventrequest.eventno -- added this line (semantic)
+ AND facname = 'Football stadium'
+ AND empname = 'Mary Manager';
+
+-- #2 (syntax, redundancy, semantic - redundancy)
+SELECT DISTINCT eventrequest.eventno, dateheld, status, estcost
+FROM eventrequest, eventplan
+WHERE estaudience > 4000
+ AND eventplan.eventno = eventrequest.eventno; 
+ -- GROUP BY eventrequest.eventno, dateheld, status, estcost; -- commented this line (redundancy)
+ 
+-- #3 (syntax, redundancy, semantic - redundancy)
+SELECT DISTINCT eventrequest.eventno, dateheld, status, estcost
+-- FROM eventrequest, employee, facility, eventplan -- commented this line (redundancy)
+FROM eventrequest, facility, eventplan
+WHERE estaudience > 5000
+ -- AND eventplan.empno = employee.empno -- commented this line (redundancy)
+ AND eventrequest.facno = facility.facno
+ AND eventplan.eventno = eventrequest.eventno
+ AND facname = 'Football stadium';
+
+-- #4 (syntax, redundancy, semantic - ?)
+SELECT DISTINCT eventrequest.eventno, dateheld, status, estcost -- fixed ambigous column [eventno]
+FROM eventrequest, employee, eventplan
+WHERE estaudience BETWEEN 5000 AND 10000 -- fixed operator BETWEEN
+AND eventplan.empno = employee.empno
+ AND eventrequest.eventno = eventplan.eventno
+ AND empname = 'Mary Manager';
+
+-- #5 (identify poor coding practices)
+SELECT eventplan.planno, lineno, resname, resourcecnt, timestart, timeend
+ FROM eventrequest, facility, eventplan, eventplanline, resourcetbl
+ WHERE estaudience = 10000
+ AND eventplan.planno = eventplanline.planno 
+ AND eventrequest.facno = facility.facno
+ AND facname = 'Basketball arena' 
+ AND eventplanline.resno = resourcetbl.resno
+ AND eventrequest.eventno = eventplan.eventno;
