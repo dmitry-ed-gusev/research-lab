22807aff88ac68d02b4c7412e4009afe0c3a04bd
Dmitrii Gusev <dmitrii.gusev@broadcom.com>
2023-03-14 17:31:00 +0100

    WIP. Working CSV format.

---
+++ documentation/Coursera. OOP in Java Specialization/java-oop-code/java-edu-app/src/main/java/edu/dmgusev/csv/ExportCSVApplication.java
@@ -0,0 +1,121 @@
+package edu.dmgusev.csv;
+
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import edu.duke.FileResource;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ExportCSVApplication {
+
+    public static final String CSV_EXPORT_FILE = "csv/exports_sample.csv";
+
+    /** Print content of the CSV file (record by record). */
+    public void tester(@NonNull String csvFile) {
+        log.debug("tester() method is working.");
+
+        FileResource fr = new FileResource(csvFile);
+        CSVParser parser = fr.getCSVParser();
+
+        for (CSVRecord csvRecord: parser) {
+            System.out.println(csvRecord);
+        }
+
+    }
+
+    /** 
+        Write a method named countryInfo that has two parameters, parser is a CSVParser and country 
+        is a String. This method returns a string of information about the country or returns “NOT FOUND” 
+        if there is no information about the country. The format of the string returned is the country, 
+        followed by “: “, followed by a list of the countries’ exports, followed by “: “, followed by the 
+        countries export value.
+
+        For example, using the file exports_small.csv and the country Germany, the program returns the 
+        string: [Germany: motor vehicles, machinery, chemicals: $1,547,000,000,000]
+    */
+    public void countryInfo(@NonNull CSVParser parser, @NonNull String country) {
+        log.debug("countryInfo() is working.");
+
+        for (CSVRecord csvRecord: parser) {
+
+            String csvCountry = csvRecord.get("Country"); // get country value
+            if (csvCountry.equalsIgnoreCase(country)) {
+
+                // get other values from CSV
+                String csvExports = csvRecord.get("Exports");
+                String csvValue   = csvRecord.get("Value (dollars)");
+
+                // print the result
+                System.out.println(
+                    String.format("%s: %s: %s", csvCountry, csvExports, csvValue));
+
+                return; // when we've found a country - immediate exit
+            }
+
+        }
+
+        System.out.println(String.format("Country [%s] NOT FOUND!", country));
+
+    }
+
+    /**
+        Write a void method named listExportersTwoProducts that has three parameters, parser is a 
+        CSVParser, exportItem1 is a String and exportItem2 is a String. This method prints the 
+        names of all the countries that have both exportItem1 and exportItem2 as export items. 
+
+        For example, using the file exports_small.csv, this method called with the items “gold” 
+        and “diamonds” would print the countries.
+    */
+    public void listExportersTwoProducts() {
+        log.debug("listExportersTwoProducts() is working.");
+    }
+
+    /** 
+        Write a method named numberOfExporters, which has two parameters, parser is a CSVParser, and 
+        exportItem is a String. This method returns the number of countries that export exportItem.
+
+        For example, using the file exports_small.csv, this method called with the item “gold” 
+        would return 3.
+    */
+    public void numberOfExporters() {
+        log.debug("numberOfExporters() is working.");
+    }
+
+    /** 
+        Write a void method named bigExporters that has two parameters, parser is a CSVParser, and amount 
+        is a String in the format of a dollar sign, followed by an integer number with a comma separator 
+        every three digits from the right. An example of such a string might be “$400,000,000”. This method 
+        prints the names of countries and their Value amount for all countries whose Value (dollars) 
+        string is longer than the amount string. You do not need to parse either string value as an 
+        integer, just compare the lengths of the strings.
+
+        For example, if bigExporters is called with 
+        the file exports_small.csv and amount with the string $999,999,999, then this method would print 
+        eight countries and their export values shown here:
+            Germany $1,547,000,000,000
+            Macedonia $3,421,000,000
+            Malawi $1,332,000,000
+            Malaysia $231,300,000,000
+            Namibia $4,597,000,000
+            Peru $36,430,000,000
+            South Africa $97,900,000,000
+            United States $1,610,000,000,000
+    */
+    public void bigExporters() {
+        log.debug("bigExporters() is working.");
+    }
+
+    /***/
+    public static void main(String[] args) {
+        log.info("Export Analisys CSV Application is starting...");
+
+        var application = new ExportCSVApplication();
+
+        log.info(String.format("Printing content of the file: [%s]%n", CSV_EXPORT_FILE));
+        application.tester(CSV_EXPORT_FILE);
+
+    }
+
+}
--- documentation/Coursera. OOP in Java Specialization/java-oop-code/java-edu-app/src/main/java/edu/dmgusev/utils/Utilities.java
+++ documentation/Coursera. OOP in Java Specialization/java-oop-code/java-edu-app/src/main/java/edu/dmgusev/utils/Utilities.java
@@ -8,89 +8,108 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
 // todo: see: https://www.baeldung.com/java-list-directory-files
 // todo: see: https://www.logicbig.com/how-to/java/list-all-files-in-resouce-folder.html
 // todo: see: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
+// todo: replace System.out with the logger!
+
+@Slf4j
 @NoArgsConstructor // (access = AccessLevel.PRIVATE)
+// we should have a public constructor as we use reference 'this' for getting current class (getClass())
 public class Utilities {
 
-    /***/
-    public static Set<String> listFilesUsingJavaIO(String dir) {
+    /** (-) Method I. List files in a given directory. Can't find relative/resources dirs. */
+    public static Set<File> listFilesUsingJavaIO(@NonNull String dir) {
+        log.info(String.format("[Java IO] Listing files for dir [%s].", dir));
+
         return Stream.of(new File(dir).listFiles())
                 .filter(file -> !file.isDirectory())
-                .map(File::getName)
+                //.map(File::getName) // <- result is Set<String> (extract files names)
                 .collect(Collectors.toSet());
+
     }
 
-    /***/
-    public static Set<String> listFilesUsingFilesList(String dir) throws IOException {
-        try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+    /** (-) Method II. List files in a given directory. Can't find relative/resources dirs. */
+    public static Set<File> listFilesUsingFilesList(@NonNull String dir) throws IOException {
+        log.info(String.format("[Files List] Listing files for dir [%s].", dir));
+
+        try (Stream<Path> stream = Files.list(Paths.get(dir))) { // try-with-resources
             return stream
                     .filter(file -> !Files.isDirectory(file))
                     .map(Path::getFileName)
-                    .map(Path::toString)
+                    //.map(Path::toString) // <- result is Set<String> (extract files names)
+                    .map(Path::toFile) // <- convert Path instance to File instance
                     .collect(Collectors.toSet());
         }
+
     }
 
-    /** This method should be qble to show [resources] folder content (list of files). */
+    /** Method III: this method should be able to show [resources] folder content (list of files). */
     public static File[] getResourceFolderFiles(String folder) {
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        URL url = loader.getResource(folder);
-        String path = url.getPath();
+        URL         url    = loader.getResource(folder);
+        String      path   = url.getPath();
         return new File(path).listFiles();
     }
 
-    /***/
-    public List<File> getAllFilesFromResource(String folder) throws URISyntaxException, IOException {
+    /** (+) Method IV: this method is able to see files in the directory inside [resources] dir.  */
+    public Set<File> getAllFilesFromResource(String folder) throws URISyntaxException, IOException {
 
-        ClassLoader classLoader = getClass().getClassLoader();
+        ClassLoader classLoader = this.getClass().getClassLoader();
         URL resource = classLoader.getResource(folder);
 
-        // dun walk the root path, we will walk all the classes
-        return Files.walk(Paths.get(resource.toURI()))
+        // do walk the root path, we will walk all the classes
+        try (Stream<Path> stream = Files.walk(Paths.get(resource.toURI()))) {
+            return stream
                 .filter(Files::isRegularFile)
                 .map(Path::toFile)
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
+        }
+
     }
 
     // print a file
-    public static void printFile(File file) {
+    public static void printFile(@NonNull File file) {
 
-        List<String> lines;
         try {
-            lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+            var lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
             lines.forEach(System.out::println);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(String.format("Can't read file: [%s].", file), e);
         }
 
     }
 
     public static void main(String[] args) throws IOException {
 
+        // todo: implement tests for all methods
+
         var app = new Utilities();
 
         // read all files from a resources folder
         try {
 
+            // methods references to static methods
+            //Function<String, Set<File>> staticMethod1 = Utilities::listFilesUsingJavaIO;
+
             // files from src/main/resources/json
-            List<File> result = app.getAllFilesFromResource("dna");
+            Set<File> result = app.getAllFilesFromResource("dna");
             for (File file : result) {
-                System.out.println("file : " + file);
-                // printFile(file);
+                log.info(String.format("Found file: [%s]", file)); // print file name
+                // printFile(file); // print file content
             }
 
         } catch (URISyntaxException | IOException e) {
-            e.printStackTrace();
+            log.error("Processing error!", e);
         }
 
     }
